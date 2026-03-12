4b7203268d23905438421fdfa6fd5cfdb4a69aa3
rahulsd380 <rahulsd380@gmail.com>
2026-03-12 11:54:07 +0600

    Changed url

--- src/redux/API/baseApi.ts
+++ src/redux/API/baseApi.ts
@@ -8,7 +8,8 @@ import type { RootState } from "../store";
 import type { BaseQueryApi, BaseQueryFn, FetchArgs } from "@reduxjs/toolkit/query";
 import type { DefinitionType } from "@reduxjs/toolkit/query";
 
-export const baseUrl = "https://bco-server.vercel.app";
+export const baseUrl = "https://api.bcoeco.com";
+// export const baseUrl = "https://bco-server.vercel.app";
 // export const baseUrl = "http://localhost:5000";
 
 const baseQuery = fetchBaseQuery({
